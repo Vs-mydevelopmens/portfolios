b9dac4d32f44d942220e7c82701a249e533a2a90
Vs-mydevelopmens <73613008+Vs-mydevelopmens@users.noreply.github.com>
2021-03-14 15:00:24 +0100

    new

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
 		$('body').toggleClass('lock');
 	});
 });
-
-
 // ==========================================
 // <ВЫДЕЛЕНИЕ АКТИВНОГО МЕНЮ>
 // ==========================================
@@ -14,51 +12,12 @@ $('a').click(function () {
     $(this).addClass('menu-active');
 });
 // ==========================================
-// $('.tabs__item').click(function () {
-//     $('.__active').removeClass('__active');
-//     $(this).addClass('__active');
-//     var tab = $(this).attr('href');
-//     $(tab).fadeIn(400);
-// });
-
-
 // ==========================================
 // </ВЫДЕЛЕНИЕ АКТИВНОГО МЕНЮ>
 // ==========================================
 
 // ==========================================
-// <WEBP IMAGE>
-// ==========================================
-function testWebP(callback) {
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-testWebP(function (support) {
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
-});
-// ==========================================
-// </WEBP IMAGE>
-// ==========================================
-
-// ==========================================
 // <2 СКРИПТА НА ИЗОБРАЖЕНИЯ, ЕСЛИ НЕ ОТОБРАЖАЕТСЯ, АКТИВНЫЙ СКРИПТ ЗАКОМЕНТИРОВАТЬ, А ЗАКОМЕНТИРОВАННЫЙ РАССКОМЕНТИРОВАТЬ>
-// ==========================================
-// function ibg() {
-//     let ibg = document.querySelectorAll(".ibg");
-//     for (var i = 0; i < ibg.length; i++) {
-//         if (ibg[i].querySelector('img')) {
-//             ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-//         }
-//     }
-// }
-// ibg();
 // ==========================================
 function ibg() {
     $.each($('.ibg'), function (index, val) {
@@ -76,7 +35,6 @@ ibg();
 // <ПОДКЛЮЧЕНИЕ ТАБОВ ПОРТФОЛИО>
 // ==========================================
 var portfolio = document.querySelector('.portfolio-content');
-
 var mixer = mixitup(portfolio);
 // ==========================================
 // </ПОДКЛЮЧЕНИЕ ТАБОВ ПОРТФОЛИО>
